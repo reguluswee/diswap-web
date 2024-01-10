8c1c0d7793a705524a09c11e858374a0e8e3189c
nnuauroral@163.com <herohero249>
2024-01-10 15:46:37 +0800

    list 修改

---
+++ src/constants/default.ts
@@ -0,0 +1,32 @@
+export const DEFAULT_LIST = {
+    "name": "DISWAP List",
+    "logoURI": "https://raw.githubusercontent.com/compound-finance/token-list/master/assets/compound-interface.svg",
+    "keywords": [
+      "compound",
+      "defi"
+    ],
+    "timestamp": "2024-01-08T20:37:00.000+00:00",
+    "tokens": [
+      {
+        "chainId": 513100,
+        "address": "0xf81ed9cecFE069984690A30b64c9AAf5c0245C9F",
+        "name": "Friend",
+        "symbol": "FREN",
+        "decimals": 18,
+        "logoURI": "https://chain.fenus.xyz/images/logo/fren.png"
+      },
+      {
+        "chainId": 513100,
+        "address": "0xF9F1234761Ae9DE3c576C5491631C6FC0d213b5E",
+        "name": "Wrapper DIS",
+        "symbol": "WDIS",
+        "decimals": 18,
+        "logoURI": "https://dischain.xyz/favicon.ico"
+      }
+    ],
+    "version": {
+      "major": 2,
+      "minor": 0,
+      "patch": 0
+    }
+}
